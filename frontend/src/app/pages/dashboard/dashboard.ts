f1a3fc8b1bba2495aca06ea1dd5e22dc7d75ca19
Abdoulaye308 <abdoulayesene.fall@unchk.edu.sn>
2026-05-20 17:58:15 +0000

    Création de dashboard moderne avec angular

--- frontend/src/app/pages/dashboard/dashboard.ts
+++ frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
 
+import { RouterLink } from '@angular/router';
+
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+
+  standalone: true,
+
+  imports: [RouterLink],
+
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+
+  styleUrl: './dashboard.css'
 })
-export class Dashboard {}
+export class Dashboard {
+
+  // Constructor
+  constructor(
+    private router: Router
+  ) {
+
+  }
+
+  // Déconnexion
+  logout() {
+
+    // Suppression token
+    localStorage.removeItem('token');
+
+    // Redirection login
+    this.router.navigate(['']);
+  }
+}
